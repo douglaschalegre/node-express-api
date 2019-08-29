b6228f0f9935356a6c980fa8a8a17300f38667bd
douglaschalegre <douglas.chalegre@gmail.com>
2019-08-29 17:24:38 -0300

    db

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = 'mongodb+srv://hookly:ST7krS5UHf2QNjjj@hooklyapi-vd574.gcp.mongodb.net/';
+const uri = 'mongodb+srv://<username>:<password>@hooklyapi-vd574.gcp.mongodb.net/';
 mongoose.connect(uri,  {dbName: 'noderest'});
 mongoose.Promise = global.Promise;
 
